ebd38e9f2a249a9e0c26cc5fd373d388ed5c1f20
Tudor <tudor.esan@icloud.com>
2023-10-20 07:12:35 +0300

    checkpoint

--- src/components/InspectorPage.js
+++ src/components/InspectorPage.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import '../styles/InspectorPage.css'; // Make sure to import the CSS file
-import Header from './Header';
-import RoundedRectangleCentre from './RoundedRectangleCentre';
-import FileUploadForm from './Zip';
+import React from "react";
+import "../styles/InspectorPage.css"; // Make sure to import the CSS file
+import Header from "./Header";
+import RoundedRectangleCentre from "./RoundedRectangleCentre";
+import FileUploadForm from "./Zip";
 
 function InspectorPage() {
   return (
     <div className="inspector-page-container">
       <Header />
-      <RoundedRectangleCentre>
+      {/* <RoundedRectangleCentre> */}
+      <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[80%] h-[80%] bg-white rounded-md">
         <FileUploadForm />
-      </RoundedRectangleCentre>
+      </div>
+      {/* </RoundedRectangleCentre> */}
     </div>
   );
 }
--- src/components/Zip.js
+++ src/components/Zip.js
@@ -1,17 +1,18 @@
 // src/components/FileUploadForm.js
 
-import React, { useRef } from 'react';
-import '../styles/Zip.css'; 
+import React, { useRef } from "react";
+import "../styles/Zip.css";
 
 function FileUploadForm() {
   const fileInputRef = useRef(null);
+  const [response, setResponse] = React.useState(null);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     // Ensure a file was selected
     if (!fileInputRef.current.files.length) {
-      alert('Please select a zip file.');
+      alert("Please select a zip file.");
       return;
     }
 
@@ -19,44 +20,57 @@ function FileUploadForm() {
 
     // Create a FormData object to hold the file data
     const formData = new FormData();
-    formData.append('file', file);
+    formData.append("file", file);
 
     try {
       // Modify the URL below to your backend endpoint
-      const response = await fetch('http://localhost:3000/inspect', {
-        method: 'POST',
-        body: formData,
-      });
+      const response = await fetch(
+        "https://9c43-86-105-110-148.ngrok-free.app/inspect",
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
 
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
       console.log(response);
       const responseData = await response.json();
       console.log(responseData);
-      alert('File uploaded successfully!'); // Handle the response data as needed
+      setResponse(responseData);
+      alert("File uploaded successfully!"); // Handle the response data as needed
     } catch (error) {
-      console.error('Error uploading file:', error);
-      alert('Error uploading file. Please try again.');
+      console.error("Error uploading file:", error);
+      alert("Error uploading file. Please try again.");
     }
   };
 
   return (
-    <div className="file-upload-container">
-      <form onSubmit={handleSubmit}>
+    <div className="">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-2 mt-8 max-w-sm justify-center items-center mx-auto"
+      >
         <label htmlFor="zipFile" className="file-upload-label">
-          Select the ZIP archive containing the SRC subfolder of your smart contract project
-          <input 
-            type="file" 
-            id="zipFile" 
-            name="zipFile" 
-            accept=".zip" 
+          Select the ZIP archive containing the SRC subfolder of your smart
+          contract project
+          <input
+            type="file"
+            id="zipFile"
+            name="zipFile"
+            accept=".zip"
             className="file-input"
             ref={fileInputRef}
           />
         </label>
-        <button type="submit" className="upload-btn">Upload</button>
+        <button type="submit" className="upload-btn">
+          Upload
+        </button>
       </form>
+      {response && (
+        <div className="flex flex-col items-center justify-center"></div>
+      )}
     </div>
   );
 }
